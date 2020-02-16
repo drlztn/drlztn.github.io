4b6669084e0cac6ff028f362ea4a090e1a57014e
Pierre Puentes aka DRLZTN <pierrepuentes@gmail.com>
2020-02-16 18:49:25 -0500

    Links materia ondulatoria

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,35 @@ function borrar_scan(){
   $("#scan").remove();
 }
 
+function init(){
+  var selection = document.querySelector('#divCollages') !== null;
+  if (selection) {
+    agregarCollages();
+    function agregarCollages () { 
+      for (var i = 1; i < 89; i++) {  
+        var currentDiv = document.getElementById("divCollages");
+        var newDiv = document.createElement('div');
+        var img = document.createElement("img");
+
+        img.src = "projects/malviaje/" + i + ".png";
+        img.setAttribute('width', '100%');
+
+        newDiv.id = i;
+        newDiv.className = "cuarto";
+        newDiv.appendChild(img);  
+
+        currentDiv.appendChild(newDiv);
+      }   
+    }
+  }
+}
+
 const swup = new Swup({
   plugins: [
-  	new SwupSlideTheme({
-  		reversed: true
-  	}), 
-  	new SwupScrollPlugin(),
+    new SwupSlideTheme({
+      reversed: true
+    }), 
+    new SwupScrollPlugin(),
     new SwupScriptsPlugin({
       head: false,
       body: true
@@ -28,3 +51,6 @@ swup.on('transitionEnd', function(){
   borrar_scan();
 });
 
+swup.on('contentReplaced', function(){
+  init();
+});

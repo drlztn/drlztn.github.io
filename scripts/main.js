608f0149efc2388938af5c5d0608eceec0e65d95
Pierre Puentes aka DRLZTN <pierrepuentes@gmail.com>
2020-02-23 20:48:56 -0500

    fancybox

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,76 @@
+//Fancybox
+lightbox();
+function lightbox(){
+	$('[data-fancybox="gallery"]').fancybox({
+		protect: true,
+		buttons : [ 
+			'share',
+			'close',
+		],
+		infobar: false
+	});
+	$.fancybox.defaults.hash = false;
+}
+
+//Borrar scan
 function borrar_scan(){
-  $("#scan").remove();
+	$("#scan").remove();
 }
 
-function init(){
-  var selection = document.querySelector('#divCollages') !== null;
-  if (selection) {
-    agregarCollages();
-    function agregarCollages () { 
-      for (var i = 1; i <= 88; i++) {  
-        var currentDiv = document.getElementById("divCollages");
-        var newDiv = document.createElement('div');
-        var img = document.createElement("img");
-
-        img.src = "projects/malviaje/" + i + ".png";
-        img.setAttribute('width', '100%');
-
-        newDiv.id = i;
-        newDiv.className = "cuarto";
-        newDiv.appendChild(img);  
-
-        currentDiv.appendChild(newDiv);
-      }   
-    }
-  }
-}
-
+//Swup
 const swup = new Swup({
-  plugins: [
-    new SwupSlideTheme({
-      reversed: true
-    }), 
-    new SwupScrollPlugin(),
-    new SwupScriptsPlugin({
-      head: false,
-      body: true
-    })
-  ]
+	plugins: [
+		new SwupSlideTheme({
+			reversed: true
+		}), 
+		new SwupScrollPlugin(),
+		new SwupScriptsPlugin({
+			head: false,
+			body: true
+		})
+	]
 });
 
 const options = {
-  linkSelector:
-    'a[href^="../"], a[href^="/"]'
+	linkSelector:
+	'a[href^="../"], a[href^="/"]'
 };
 
 swup.on('willReplaceContent', function(){
-  borrar_scan();
+	borrar_scan();
 });
 
 swup.on('transitionEnd', function(){
-  borrar_scan();
+	borrar_scan();
 });
 
 swup.on('contentReplaced', function(){
-  init();
+	collages(); //Sólo en un malviaje en tabogo
+	lightbox();
 });
+
+//Agregar collages
+function collages(){
+	var selection = document.querySelector('#divCollages') !== null;
+	if (selection) {
+		agregarCollages();
+		function agregarCollages () { 
+			for (var i = 1; i <= 88; i++) {  
+				var currentDiv = document.getElementById("divCollages");
+				var newDiv = document.createElement('a');
+				var img = document.createElement("img");
+
+				img.src = "projects/malviaje/" + i + ".png";
+				img.setAttribute('width', '100%');
+
+				newDiv.href = "projects/malviaje/" + i + ".png";
+				newDiv.id = i;
+				newDiv.className = "cuarto";
+				newDiv.setAttribute('data-fancybox', 'gallery'); 
+				newDiv.appendChild(img); 
+
+				currentDiv.appendChild(newDiv);
+			}
+		}
+	}
+}
---
+++ scripts/menu.js
@@ -0,0 +1,40 @@
+function myFunction(x) {
+	if (menu_celu.matches) { // If media query matches
+	document.getElementById("menu").innerHTML =
+	'<a href="/index.html">'+
+	'<img src="resources/images/logo_200.png" style="height: 120px; ">'+
+	'</a>'+
+	'<p style="font-size: 9pt">'+
+		'<a class="link_normal" href="/about.html">PIERRE PUENTES </a>'+
+		'AKA '+
+		'<a class="link_normal" href="/derealization.html"> DRLZTN</a>'+
+	'</p>'+
+	'<p style="font-size: 9pt">'+
+		'<a class="link_normal" href="/works.html">OTROS PROYECTOS</a>'+
+		'					   '+
+		'<a class="link_normal" href="https://derealizationnn.tumblr.com/">BLOG</a>'+
+		'					   '+
+		'<a class="link_normal" href="/contact.html">CONTACTO</a>'+
+	'</p>';
+	}	else {
+	document.getElementById("menu").innerHTML = 
+	'<a href="/index.html">'+
+	'<img src="resources/images/logo_200.png" style="height: 120px; ">'+
+	'</a>'+
+	'<p style="font-size: 9pt">'+
+		'<a class="link_normal" href="/about.html">PIERRE PUENTES</a>'+
+		'<br>AKA'+
+		'<a class="link_normal" href="/derealization.html"> DRLZTN</a>'+
+	'</p>'+
+	'<p style="font-size: 9pt">'+
+		'<a class="link_normal" href="/works.html">OTROS PROYECTOS</a>'+
+		'<br>'+
+		'<a class="link_normal" href="https://derealizationnn.tumblr.com/">BLOG</a>'+
+		'<br>'+
+		'<a class="link_normal" href="/contact.html">CONTACTO</a>'+
+	'</p>';
+		}
+}
+var menu_celu = window.matchMedia("(max-width: 480px)")
+myFunction(menu_celu)
+menu_celu.addListener(myFunction)
